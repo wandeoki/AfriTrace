086b9ef4ed3be69f8abe41ef9ca03c7fa7931cba
Dayo-Adewuyi <dayoadewuyi@protonmail.com>
2024-09-11 20:21:37 +0100

    fix

--- afritrace-app/src/App.js
+++ afritrace-app/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
--- afritrace-app/src/index.js
+++ afritrace-app/src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000,
-    },
-  },
-});
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 60 * 1000,
+//     },
+//   },
+// });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+  
       <App />
-    </QueryClientProvider>
+    
   </React.StrictMode>
 );
 
