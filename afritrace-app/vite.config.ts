22e9b3a51693f98081f455ab15876398c1b822c6
Dayo-Adewuyi <dayoadewuyi@protonmail.com>
2024-09-12 06:00:59 +0100

    t

--- afritrace-app/vite.config.ts
+++ afritrace-app/vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['graphql']
+    }
+  }
 })
